27c92e7305eb2761bac17da5d64f4a80b876a925
rahuleth <rahulsaini593@gmail.com>
2025-03-03 13:09:30 +0530

    all step defination executed for HSC

--- features/consensus.feature
+++ features/consensus.feature
@@ -5,11 +5,11 @@ Feature: Use Cases for the consensus service and Threshold keys
     When A topic is created with the memo "Taxi rides" with the first account as the submit key
     And The message "Ride from A to B" is published to the topic
     Then The message "Ride from A to B" is received by the topic and can be printed to the console
-
+  @dev  
   Scenario: Create a topic and publish a message with a threshold key
-    Given a first account with more than 10 hbars
+    Given the first account with more than 10 hbars
     And A second account with more than 10 hbars
     And A 1 of 2 threshold key with the first and second account
     When A topic is created with the memo "Taxi rides" with the threshold key as the submit key
-    And The message "Ride from A to B" is published to the topic
-    Then The message "Ride from A to B" is received by the topic and can be printed to the console
+    And A message "Ride from A to B" is published to the topic
+    Then A message "Ride from A to B" is received by the topic and can be printed to the console
--- features/step_definitions/create-simple-topic.ts
+++ features/step_definitions/create-simple-topic.ts
@@ -1,4 +1,4 @@
-import { Given, Then, When } from "@cucumber/cucumber";
+import { Given, Then, When, setDefaultTimeout } from "@cucumber/cucumber";
 import {
   AccountBalanceQuery,
   AccountId,
@@ -12,6 +12,7 @@ import { accounts } from "../../src/config";
 import assert from "node:assert";
 import ConsensusSubmitMessage = RequestType.ConsensusSubmitMessage;
 
+setDefaultTimeout(10 * 1000);
 // Pre-configured client for test network (testnet)
 const client = Client.forTestnet()
 
@@ -25,7 +26,7 @@ Given(/^a first account with more than (\d+) hbars$/, async function (expectedBa
   this.privKey = privKey
   client.setOperator(this.account, privKey);
 
-//Create the query request
+  //Create the query request
   const query = new AccountBalanceQuery().setAccountId(account);
   const balance = await query.execute(client)
   assert.ok(balance.hbars.toBigNumber().toNumber() > expectedBalance)
@@ -33,88 +34,85 @@ Given(/^a first account with more than (\d+) hbars$/, async function (expectedBa
 
 
 When(/^A topic is created with the memo "([^"]*)" with the first account as the submit key$/, async function (memo: string) {
-    // const tx =  await new TopicCreateTransaction()
-    // .setSubmitKey(this.privKey)
-    // .setTopicMemo(memo)
-    // .execute(client);
-    // const receipt = await tx.getReceipt(client);
-    // const newTopicId = receipt.topicId;
-    // console.log(`New HCS topic ID: ${newTopicId}`)
-    this.topicId = '0.0.5635773' //newTopicId;
-    // Wait 5 seconds between consensus topic creation and subscription creation
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
+  const tx =  await new TopicCreateTransaction()
+  .setSubmitKey(this.privKey)
+  .setTopicMemo(memo)
+  .execute(client);
+  const receipt = await tx.getReceipt(client);
+  const newTopicId = receipt.topicId;
+  console.log(`New HCS topic ID: ${newTopicId}`)
+  this.topicId = newTopicId;
+
 });
 
 When(/^The message "([^"]*)" is published to the topic$/, async function (message: string) {
+  await new Promise((resolve) => setTimeout(resolve, 3000))
   new TopicMessageQuery()
-  .setTopicId(this.topicId)
-  .subscribe(client, null, (message) => {
-    let messageAsString = Buffer.from(message.contents).toString();
-    console.log("messageAsString------- 53", messageAsString)
-    console.log(
-      `${message.consensusTimestamp.toDate()} Received: ${messageAsString}`
-    );
-  });
+    .setTopicId(this.topicId)
+    .subscribe(client, null, (message) => {
+      let messageAsString = Buffer.from(message.contents).toString();
+      console.log("line 53: subscribe published message ------- ", messageAsString)
+      console.log(
+        `${message.consensusTimestamp.toDate()} Received: ${messageAsString}`
+      );
+    });
   // Send message to the topic
   // await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    let messageTx = await new TopicMessageSubmitTransaction()
-      .setTopicId(this.topicId)
-      .setMessage(message)
-      .freezeWith(client);
-  
-    const signedTx = await messageTx.sign(this.privKey);
-    const sendRespone = await signedTx.execute(client)  
-    // Get the receipt of the transaction
-    const getReceipt = await sendRespone.getReceipt(client);
-  
-    // Get the status of the transaction
-    const transactionStatus = getReceipt.status
-    console.log("The message transaction status " + transactionStatus.toString()) 
+  let messageTx = await new TopicMessageSubmitTransaction()
+    .setTopicId(this.topicId)
+    .setMessage(message)
+    .freezeWith(client);
+
+  const signedTx = await messageTx.sign(this.privKey);
+  const sendRespone = await signedTx.execute(client)
+  // Get the receipt of the transaction
+  const getReceipt = await sendRespone.getReceipt(client);
+
+  // Get the status of the transaction
+  const transactionStatus = getReceipt.status
+  console.log("line 73: The message transaction status " + transactionStatus.toString())
 });
 
 Then(/^The message "([^"]*)" is received by the topic and can be printed to the console$/, async function (message: string) {
   // Subscribe to the topic
-  console.log('----- line 77 ', this.topicId)
   new TopicMessageQuery()
-  .setTopicId(this.topicId.toString())
-  .subscribe(client, null, (message) => {
-    console.log('-------', message)
-    let messageAsString = Buffer.from(message.contents).toString();
-    console.log("messageAsString------- ", messageAsString)
-    console.log(
-      `${message.consensusTimestamp.toDate()} Received: ${messageAsString}`
-    );
-  });
+    .setTopicId(this.topicId.toString())
+    .subscribe(client, null, (message) => {
+      let messageAsString = Buffer.from(message.contents).toString();
+      console.log("line 82: Subscribe published message -------", messageAsString)
+      console.log(
+        `${message.consensusTimestamp.toDate()} Received: ${messageAsString}`
+      );
+    });
 });
 
-// Given(/^a first account with more than (\d+) hbars$/, async function (balance: number) {
-//   const acc = accounts[0]
-//   const accountId: AccountId = AccountId.fromString(acc.id);
-//   const privKey: PrivateKey = PrivateKey.fromStringED25519(acc.privateKey);
-//   client.setOperator(this.account, privKey);
+Given(/^the first account with more than (\d+) hbars$/, async function (balance: number) {
+  const acc = accounts[0]
+  const accountId: AccountId = AccountId.fromString(acc.id);
+  const privKey: PrivateKey = PrivateKey.fromStringED25519(acc.privateKey);
+  client.setOperator(accountId, privKey);
 
-// //Create the query request
-//   const query = new AccountBalanceQuery().setAccountId(accountId);
-//   const bal = await query.execute(client)
-//   console.log("balance--------line 101 ", bal.hbars.toBigNumber().toNumber())
-//   assert.ok(bal.hbars.toBigNumber().toNumber() > balance)
-// });
+  //Create the query request
+  const query = new AccountBalanceQuery().setAccountId(accountId);
+  const bal = await query.execute(client)
+  console.log("line 99: balance-------- ", bal.hbars.toBigNumber().toNumber())
+  assert.ok(bal.hbars.toBigNumber().toNumber() > balance)
+});
 Given(/^A second account with more than (\d+) hbars$/, async function (expectedBalance: number) {
   const acc = accounts[1]
-  const account: AccountId = AccountId.fromString(acc.id);
+  const accountId: AccountId = AccountId.fromString(acc.id);
   const privKey: PrivateKey = PrivateKey.fromStringED25519(acc.privateKey);
-  client.setOperator(this.account, privKey);
+  client.setOperator(accountId, privKey);
 
-//Create the query request
-  const query = new AccountBalanceQuery().setAccountId(account);
+  //Create the query request
+  const query = new AccountBalanceQuery().setAccountId(accountId);
   const balance = await query.execute(client)
-  console.log("balance--------line 112 ", balance.hbars.toBigNumber().toNumber())
+  console.log("line 109: balance----: ", balance.hbars.toBigNumber().toNumber())
   assert.ok(balance.hbars.toBigNumber().toNumber() > expectedBalance)
 });
 
-Given(/^A (\d+) of (\d+) threshold key with the first and second account$/, async function () {
-  console.log('-----threshold key account-------')
+Given(/^A (\d+) of (\d+) threshold key with the first and second account$/, async function (num1: number, num2: number) {
   const acc1 = accounts[0]
   const privKey1: PrivateKey = PrivateKey.fromStringED25519(acc1.privateKey);
   const acc2 = accounts[1]
@@ -122,22 +120,75 @@ Given(/^A (\d+) of (\d+) threshold key with the first and second account$/, asyn
   const thresholdKey = new KeyList([privKey1.publicKey, privKey2.publicKey], 1);
   this.thresholdKey = thresholdKey;
   const accountTx = new AccountCreateTransaction()
-  .setKey(thresholdKey)
-  .setInitialBalance(new Hbar(10))// initial balance
-  .execute(client);
-
+    .setKey(thresholdKey)
+    .setInitialBalance(new Hbar(10))// initial balance
+    .execute(client);
+  
   const receipt = await (await accountTx).getReceipt(client);
   const accountId = receipt.accountId;
-  console.log(` New Account Created with 1-of-2 Threshold Key: ${accountId}`);
+  this.accountId = accountId;
+  console.log(`line 128: New Account Created with 1-of-2 Threshold Key: ${accountId}`);
 });
 
 When(/^A topic is created with the memo "([^"]*)" with the threshold key as the submit key$/, async function (memo: string) {
-    console.log('topicId with threshold key------')
-    const tx =  await new TopicCreateTransaction()
+  const acc1 = accounts[0]
+  const privKey1 = PrivateKey.fromStringED25519(acc1.privateKey)
+  const tx = await new TopicCreateTransaction()
     .setSubmitKey(this.thresholdKey)
     .setTopicMemo(memo)
-    .execute(client);
-    const receipt = await tx.getReceipt(client);
-    const newTopicId = receipt.topicId;
-    console.log(`New HCS topic ID for threshold key: ${newTopicId}`)
+    .freezeWith(client)
+    .sign(privKey1);
+
+  const resp = await tx.execute(client)
+  const receipt = await resp.getReceipt(client);
+  const newTopicId = receipt.topicId;
+  this.topicId = newTopicId;
+  console.log(`line 144: New HCS topic ID for threshold key: ${this.topicId}`)
+});
+
+
+When(/^A message "([^"]*)" is published to the topic$/, async function (message: string) {
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+  new TopicMessageQuery()
+    .setTopicId(this.topicId)
+    .subscribe(client, null, (message) => {
+      let messageAsString = Buffer.from(message.contents).toString();
+      console.log("line 153: Subscribe to published message --", messageAsString)
+      console.log(
+        `${message.consensusTimestamp.toDate()} Received: ${messageAsString}`
+      );
+    });
+  // Send message to the topic
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  const acc1 = accounts[0]
+  const privKey1 = PrivateKey.fromStringED25519(acc1.privateKey)
+  let messageTx = await new TopicMessageSubmitTransaction()
+    .setTopicId(this.topicId.toString())
+    .setMessage(message)
+    .freezeWith(client);
+
+  const signedTx = await messageTx.sign(privKey1);
+  const sendRespone = await signedTx.execute(client)
+  // Get the receipt of the transaction
+  const getReceipt = await sendRespone.getReceipt(client);
+
+  // Get the status of the transaction
+  const transactionStatus = getReceipt.status
+  console.log("line 174: The message transaction status ------  " + transactionStatus.toString())
+});
+
+Then(/^A message "([^"]*)" is received by the topic and can be printed to the console$/, async function (message: string) {
+  // Subscribe to the topic
+  const acc1 = accounts[0]
+  const privKey1 = PrivateKey.fromStringED25519(acc1.privateKey)
+  const accountId1: AccountId = AccountId.fromString(acc1.id);
+  client.setOperator(accountId1, privKey1)
+  new TopicMessageQuery()
+    .setTopicId(this.topicId.toString())
+    .subscribe(client, null, (message) => {
+      let messageAsString = Buffer.from(message.contents).toString();
+      console.log(
+        `${message.consensusTimestamp.toDate()} Received: ${messageAsString}`
+      );
+    });
 });
